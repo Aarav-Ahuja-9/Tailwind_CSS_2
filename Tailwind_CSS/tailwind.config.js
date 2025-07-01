a2bdc600d36c9132683856142d819d77a355c0ec
Aarav Ahuja <mr.aaravahuja@gmail.com>
2025-07-01 06:14:14 +0000

    refactor: reorganize Tailwind CSS setup and add configuration files

---
+++ Tailwind_CSS/tailwind.config.js
@@ -0,0 +1,10 @@
+// Tailwind_CSS/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
